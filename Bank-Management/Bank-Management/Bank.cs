6edbf5f2c30057af6a18c7dfaaa13ded6f0e2f34
Moshe-atlani <atlani.moshe@gmail.com>
2020-05-14 10:24:48 +0300

    first commit

---
+++ Bank-Management/Bank-Management/Bank.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Bank_Management
+{
+    internal class Bank
+    {
+        public Bank()
+        {
+        }
+
+        public void ShowMenu()
+        {
+            Console.WriteLine("menu");
+        }
+    }
+}
---
+++ Bank-Management/Bank-Management/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_Management
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // קוד שישתמש במחלקות שלך
+
+            Bank bank = new Bank();
+            bool exit = false;
+            while(!exit)
+            {
+                bank.ShowMenu();
+                Console.WriteLine("Please enter your choice: ");
+                int choice = int.Parse(Console.ReadLine());
+                if (choice == 1)
+                {
+                    Console.WriteLine();
+                }
+                else if(choice == 2)
+                {
+
+                }
+                else if (choice == 5)
+                {
+                    Console.WriteLine("Exiting...");
+                    exit = true;
+                }
+                else
+                {
+                    Console.WriteLine("Error: Invalid choice: " + choice);
+                }
+            }
+        }
+    }
+}
